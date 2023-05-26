020967869f3f0b9fd18eb382d207cfb414306eb3
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-05-26 16:24:23 +0100

    update package.json

--- src/api/emojis.ts
+++ src/api/emojis.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import Comment from './comments/comment.model';
+import { v4 as uuidV4 } from 'uuid'
 const router = express.Router();
 
 type EmojiResponse = string[];
@@ -7,5 +8,11 @@ type EmojiResponse = string[];
 router.get<{}, EmojiResponse>('/', (req, res) => {
   res.json(['😀', '😳', '🙄']);
 });
+
+router.get<{}, Comment>('/comment', (req, res) => {
+  let comment = { id: uuidV4(), content: "hello world", createdAt: new Date, likeCount: 0 }
+  comment = Comment.parse(comment)
+  res.json(comment)
+})
 
 export default router;
