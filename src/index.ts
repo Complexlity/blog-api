59f982531914fac501d96be480115542ff5e68dd
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-08-13 22:47:05 +0100

    feat: change default port to 3000

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import app from './app';
 import './utils/db'
 import { log } from './utils/logger';
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   /* eslint-disable no-console */
   log.info(`Listening: http://localhost:${port}`);
